d5939e31b7bb9c99f01e411456cce90d3740e682
Xe-Aadrit <bhattacharyaaadrit0@gmail.com>
2025-12-31 05:18:58 +0530

    Add HCF recursive function

--- python/code02.py
+++ python/code02.py
@@ -1,6 +1,6 @@
 # Recursion practice
 
-# Fibonacci Series using recursion
+# 1. Fibonacci Series using recursion
 # n = number of the term
 def fibo(n):
     if n == 1:
@@ -9,17 +9,24 @@ def fibo(n):
         return 1
     return fibo(n - 1) + fibo(n - 2)
 
-# Sum of digits of a number
+# 2. Sum of digits of a number
 def sum_of_digits(n):
     if n == 0:
         return 0
     return (n % 10) + sum_of_digits(n // 10)
 
-# Factorial of a number
+# 3. Factorial of a number
 def fact(n):
     if n == 0:
         return 1
     return n * fact(n - 1)
+
+# 4. HCF of 2 numbers
+def hcf(a, b):
+    r = a % b
+    if r == 0:
+        return b
+    return hcf(b, a%b)
 
 # User-input validator
 def validate_number(user_input):
@@ -34,7 +41,7 @@ def validate_number(user_input):
 print("\ncode02/Aeon")
 while True:
     print("\n------------ Menu ------------")
-    print(" 1. Nth term of Fibonacci series\n 2. Sum of digits\n 3. Factorial\n 4. Exit\n")
+    print(" 1. Nth term of Fibonacci series\n 2. Sum of digits\n 3. Factorial\n 4. HCF\n 5. Exit\n")
     choice = input("Enter your choice: ")
     match choice:
         case '1':
@@ -71,6 +78,27 @@ while True:
             print(f"Sum of digits of {validated_num} is {fact(validated_num)}.")
 
         case '4':
+            # HCF of two numbers
+            # Number 1
+            x = input("Enter number 1: ")
+            validated_num1 = validate_number(x)
+
+            # Validate first number
+            if validated_num1 == None:
+                continue
+
+            # Number 2
+            y = input("Enter number 2: ")
+            validated_num2 = validate_number(y)
+
+            # Validate second number
+            if validated_num2 == None:
+                continue
+
+            hcf_of_numbers = hcf(validated_num1, validated_num2)
+            print(f"HCF of {validated_num1} and {validated_num2} is {hcf_of_numbers}")
+
+        case '5':
             # Exit the code
             print("Exiting...")
             break
